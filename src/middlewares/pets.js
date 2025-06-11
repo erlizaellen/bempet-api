ecea1560c9dbeffb5e1f7d86726cc73497cf9776
ramirospinheiro <ramiropinheiro97@gmail.com>
2025-06-11 11:44:50 -0300

    init pets

--- src/middlewares/pets.js
+++ src/middlewares/pets.js
@@ -1,7 +1,7 @@
 function validateCreatePet(req, res, next){
     const {tutor, nome, raca, nascimento, peso} = req.body;
 
-    if (!tutor ||!nome || !raca || !nascimento || !peso){
+    if (!tutor || !nome || !raca || !nascimento || !peso){
         return res.status(400).send('Todos os campos sao obrigatorios')
     }
     if (nome.length > 100){
@@ -10,6 +10,7 @@ function validateCreatePet(req, res, next){
     if (raca.length > 50){
         return res.status(400).send('Raça do pet não pode ter mais de 50 caracteres')
     }
+    
 
     next();
 } 
