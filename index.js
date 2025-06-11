90371ce5adeb278199e22f342488671e0d38ce5f
ramirospinheiro <ramiropinheiro97@gmail.com>
2025-06-11 10:27:21 -0300

    esqueleto

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const express = require ('express')
 const app = express()
+const port = 6579
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req,res)=>{
+    res.send("Ola mundo!")
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
