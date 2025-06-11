92b55c05b73dcfb84ff1a00a0f815273cf7b30e4
ramirospinheiro <ramiropinheiro97@gmail.com>
2025-06-11 11:28:30 -0300

    initi pets

---
+++ src/middlewares/pets.js
@@ -0,0 +1,28 @@
+function validateCreatePet(req, res, next){
+    const {tutor, nome, raca, nascimento, peso} = req.body;
+
+    if (!tutor ||!nome || !raca || !nascimento || !peso){
+        return res.status(400).send('Todos os campos sao obrigatorios')
+    }
+    if (nome.length > 100){
+        return res.status(400).send('Nome do pet não pode ter mais de 100 caracteres')
+    }
+    if (raca.length > 50){
+        return res.status(400).send('Raça do pet não pode ter mais de 50 caracteres')
+    }
+
+    next();
+} 
+
+function validateDeletePet(req, res, next){
+    const {id} = req.params;
+    if (!id) {
+        return res.status(400).send('Id do produto é obrigatoria')
+    }
+    next();
+}
+
+module.exports = {
+    validateCreatePet,
+    validateDeletePet
+}
