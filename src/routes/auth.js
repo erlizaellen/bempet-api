b25e241c9c3794bfbb049b0eba4a572367f82616
erliza ellen <erlizaellen@gmail.com>
2025-06-17 11:00:22 -0300

    post do login com dois N retirado ok

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth');
 
-router.post('/loginn', authController.login);
+router.post('/login', authController.login);
 
 module.exports = router
